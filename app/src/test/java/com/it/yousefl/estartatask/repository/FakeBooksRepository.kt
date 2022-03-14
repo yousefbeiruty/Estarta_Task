ed3de1df832bbbb4e37f8a7f2a8a31c85e542c29
yousefbeiruty <yousefbeiruty@hotmail.com>
2022-03-14 20:33:15 +0300

    Made some changes.

--- app/src/test/java/com/it/yousefl/estartatask/repository/FakeBooksRepository.kt
+++ app/src/test/java/com/it/yousefl/estartatask/repository/FakeBooksRepository.kt
@@ -1,6 +1,7 @@
 package com.it.yousefl.estartatask.repository
 
 import androidx.lifecycle.MutableLiveData
+import com.google.gson.annotations.SerializedName
 import com.it.yousefl.estartatask.data.remote.booksresponse.Book
 import com.it.yousefl.estartatask.ui.main.mainrepository.BooksRepository
 import com.it.yousefl.estartatask.utils.Resource
@@ -21,92 +22,17 @@ class FakeBooksRepository : BooksRepository {
         observableEventItems.postValue(eventItems)
     }
 
-//    override suspend fun convertDate(date: String): Resource<DateModel> {
-//
-//        return if (shouldReturnANetworkError) {
-//            Resource.error("Error", null)
-//        } else {
-//            Resource.success(
-//                DateModel(
-//                    200,
-//                    Data(
-//                        Gregorian(
-//                            "date",
-//                            "day",
-//                            Designation(
-//                                "abbreviated",
-//                                "expanded"
-//                            ),
-//                            "format",
-//                            Month(
-//                                "en",
-//                                1
-//                            ),
-//                            Weekday("en"),
-//                            "year"
-//                        ),
-//                        Hijri(
-//                            "date",
-//                            "day",
-//                            DesignationX(
-//                                "abbreviated",
-//                                "expanded"
-//                            ),
-//                            "format",
-//                            ArrayList<String>(),
-//                            MonthX(
-//                                "ar",
-//                                "en",
-//                                1
-//                            ),
-//                            WeekdayX(
-//                                "ar",
-//                                "en"
-//                            ),
-//                            "year"
-//                        )
-//                    ),
-//                    ""
-//                )
-//            )
-//        }
-//    }
-//
-//    override suspend fun insertEvent(eventItem: EventItem) {
-//       eventItems.add(eventItem)
-//        refreshLiveData()
-//    }
-//
-//    override suspend fun deleteEvent(eventItem: EventItem) {
-//        eventItems.remove(eventItem)
-//        refreshLiveData()
-//    }
-//
-//    override suspend fun deleteAllEvents() {
-//        eventItems.clear()
-//        refreshLiveData()
-//    }
-//
-//    override suspend fun update(eventItem: EventItem) {
-//       eventItems.remove(eventItem)
-//        refreshLiveData()
-//    }
-//
-//    override suspend fun deleteDataEvent(idList: List<Int>) {
-//        idList.map {
-//            eventItems.map { event->
-//                if(it==event.id)
-//                    eventItems.remove(event)
-//            }
-//        }
-//        refreshLiveData()
-//    }
-//
-//    override fun observeAllEventItems(): LiveData<List<EventItem>> {
-//        return observableEventItems
-//    }
 
     override suspend fun getBooks(): Resource<List<Book>> {
-        TODO("Not yet implemented")
+        val s = mutableListOf<String>()
+        return if (shouldReturnANetworkError) {
+            Resource.error("Error", null)
+        } else {
+            val m = mutableListOf<Book>()
+            m.add(
+                Book("Date", s, s, s, "Name", "Price", "UID")
+            )
+            Resource.success(m)
+        }
     }
 }
